cbf664526746132c3b7b1049819f06720d1d2eff
Ifrah Hussein <Ifrahhssn@outlook.com>
2021-09-05 19:29:18 +0100

    Added place_order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,10 +2,17 @@ class Takeaway
   attr_reader :menu
 
   def initialize
-    @menu = [{ :fish => 7.8 }, { :chips => 2.1 }]
+    @menu = [{ :fish => 7.8 }, { :pie => 5.0 }, 
+        { :chips => 2.1 }, { :mushypeas => 1.5 }, 
+        { :currysauce => 1.0 }, { :gravy => 1.0 }]
+       
   end
   
   def display_menu
     @menu     
+  end
+
+  def place_order
+
   end
 end 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,7 +6,13 @@ describe Takeaway do
   
   describe '#display_menu' do
     it 'displays menu' do
-      expect(takeaway.display_menu).to eq [{ :fish => 7.8 }, { :chips => 2.1 }]
+      expect(takeaway.display_menu).to eq [{ :fish => 7.8 }, { :pie => 5.0 }, { :chips => 2.1 }, { :mushypeas => 1.5 }, { :currysauce => 1.0 }, { :gravy => 1.0 }]
     end  
   end
+
+  describe '#place_order' do
+    it 'responds to place_order' do
+      expect(takeaway).to respond_to(:place_order)
+    end
+  end  
 end
